06db8bb68683fc675342bf67a6bbc23d54ba4a63
Shuny Networks <shunynetworks@gmail.com>
2021-03-26 15:25:23 +0530

    Parivahan Inc. Backend Server Cors Headers Addition and some other Stuff

--- server.js
+++ server.js
@@ -33,14 +33,32 @@ app.use(express.static(__dirname + "./data/"));
 // Data Parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// Cookie Parsing
+app.use(cookieParser());
+// Cors Error
 app.use(Cors({
     origin: "http://localhost:3000",
     preflightContinue: true,
     credentials: true,
 }));
 
-// Cookie Parsing
-app.use(cookieParser());
+app.use(function(req, res, next) {
+    // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  
+  // Pass to next layer of middleware
+  next();
+  });
 
 // PassportJS
 app.use(passport.initialize());
