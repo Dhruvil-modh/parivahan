df63004b0f234749f3a34fda09dc9e2de8a65e4d
Shuny Networks <shunynetworks@gmail.com>
2021-03-26 17:01:38 +0530

    Parivahan Inc. Backend Server Cors Headers Addition and some other Stuff for Production

--- server.js
+++ server.js
@@ -37,28 +37,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 // Cors Error
 app.use(Cors({
-    origin: "http://localhost:3000",
+    origin: "https://parivahan.tech",
     preflightContinue: true,
     credentials: true,
 }));
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  
-  // Pass to next layer of middleware
-  next();
-  });
+    res.setHeader('Access-Control-Allow-Origin', 'https://parivahan.tech');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // PassportJS
 app.use(passport.initialize());
